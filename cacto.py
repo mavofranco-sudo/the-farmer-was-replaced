1db26c38f6ea0ee43269618696ce8342fb7eff9a
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 16:38:50 -0300

    fix: _measure_safe trata None de measure() em celulas vazias no insertion_sort

--- cacto.py
+++ cacto.py
@@ -13,9 +13,18 @@ def _planta_cacto():
 	if num_items(Items.Water) > 0:
 		use_item(Items.Water)
 
+def _measure_safe(direcao=None):
+	if direcao == None:
+		val = measure()
+	else:
+		val = measure(direcao)
+	if val == None:
+		return 0
+	return val
+
 def insertion_sort(direcao, planta=False):
 	x, y = get_pos_x(), get_pos_y()
-	
+
 	if planta:
 		_planta_cacto()
 
@@ -26,7 +35,7 @@ def insertion_sort(direcao, planta=False):
 			_planta_cacto()
 
 		j = i
-		while j > 0 and measure(direcao) > measure():
+		while j > 0 and _measure_safe(direcao) > _measure_safe():
 			swap(direcao)
 			move(direcao)
 			j -= 1
@@ -40,7 +49,7 @@ def cria_insertion_sort(direcao, planta=False):
 def modo_cacto(objetivo):
 	while gerenciador.precisa(Items.Cactus, objetivo):
 		megafazenda.paraleliza_linha(cria_insertion_sort(West, True))
-		megafazenda.paraleliza_linha(cria_insertion_sort(South), False)
+		megafazenda.paraleliza_linha(cria_insertion_sort(South, False))
 
 		campo.vai_para(0, 0)
 		harvest()
