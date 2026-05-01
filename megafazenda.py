9e5cce66fa80eff1f7c3645b316614fe391f6eb7
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 18:43:25 -0300

    fix: paraleliza_blocos cobre campo inteiro com while+step, teto_div para blocos sem borda cega

--- megafazenda.py
+++ megafazenda.py
@@ -26,18 +26,26 @@ def inicializa():
 	if drones_por_coluna < 1:
 		drones_por_coluna = 1
 
-	linhas = campo.n // drones_por_linha
+	# bloco de cada drone (arredondado para cima para cobrir campo todo)
+	linhas = util.teto_div(campo.n, drones_por_linha)
 	if linhas < 1:
 		linhas = 1
 
-	colunas = campo.n // drones_por_coluna
+	colunas = util.teto_div(campo.n, drones_por_coluna)
+	# colunas deve ser par para movimento_bloco funcionar corretamente
 	if colunas % 2 == 1:
-		colunas -= 1
+		colunas += 1
 	if colunas < 1:
 		colunas = 1
 
-	_limite_linhas = linhas * drones_por_linha
-	_limite_colunas = colunas * drones_por_coluna
+	# limites: quantas celulas o grid de drones cobre
+	# nao pode ultrapassar campo.n
+	_limite_linhas = min(linhas * drones_por_linha, campo.n)
+	_limite_colunas = min(colunas * drones_por_coluna, campo.n)
+
+	print("    [mega] n=" + str(campo.n) + " drones=" + str(n_drones) +
+		" bloco=" + str(colunas) + "x" + str(linhas) +
+		" cobertura=" + str(_limite_colunas) + "x" + str(_limite_linhas))
 
 def paraleliza_linha(acao, por_linha=True):
 	drones = []
@@ -64,15 +72,18 @@ def paraleliza_blocos(acao):
 	drones = []
 	resultados = []
 
-	for x in range(0, _limite_colunas, colunas):
-		for y in range(0, _limite_linhas, linhas):
+	x = 0
+	while x < campo.n:
+		y = 0
+		while y < campo.n:
 			campo.vai_para(x, y)
-
 			drone = spawn_drone(chapeus.usa_e_faz(acao))
 			if drone:
 				drones.append(drone)
 			else:
 				resultados.append(chapeus.usa_e_faz(acao)())
+			y += linhas
+		x += colunas
 
 	for drone in drones:
 		resultados.append(wait_for(drone))
