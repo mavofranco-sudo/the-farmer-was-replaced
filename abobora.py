0855a6fbaececcc8ade9313e8bd39cb86c76899d
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 18:01:20 -0300

    fix: abobora nao colhe individualmente - espera campo todo maduro para bonus mega-abobora

--- abobora.py
+++ abobora.py
@@ -2,7 +2,7 @@ import campo
 import gerenciador
 import megafazenda
 
-def _tarefa_ciclo():
+def _tarefa_planta_e_cuida():
 	def funcao():
 		def trata_celula():
 			tipo = get_entity_type()
@@ -18,9 +18,7 @@ def _tarefa_ciclo():
 					plant(Entities.Pumpkin)
 				campo._agua()
 			elif tipo == Entities.Pumpkin:
-				if can_harvest():
-					harvest()
-				else:
+				if not can_harvest():
 					campo._agua()
 		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, trata_celula)
 	return funcao
@@ -38,6 +36,14 @@ def _todas_prontas():
 				return False
 	return True
 
+def _tarefa_colhe_tudo():
+	def funcao():
+		def colhe():
+			if get_entity_type() == Entities.Pumpkin and can_harvest():
+				harvest()
+		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, colhe)
+	return funcao
+
 def _reabastece():
 	n_celulas = campo.n * campo.n
 	custo_por_semente = 512
@@ -52,23 +58,15 @@ def _reabastece():
 	if num_items(Items.Carrot) < minimo_cenouras:
 		gerenciador.farma_recurso(Items.Carrot, minimo_cenouras)
 
-def _tarefa_colhe_tudo():
-	def funcao():
-		def colhe():
-			if get_entity_type() == Entities.Pumpkin and can_harvest():
-				harvest()
-		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, colhe)
-	return funcao
-
 def modo_abobora(objetivo):
 	campo.ara()
 	while gerenciador.precisa(Items.Pumpkin, objetivo):
 		_reabastece()
 		# planta o campo todo
-		megafazenda.paraleliza_blocos(_tarefa_ciclo())
-		# aguarda crescimento - a cada passagem rega e replanta podres
+		megafazenda.paraleliza_blocos(_tarefa_planta_e_cuida())
+		# aguarda: rega e replanta podres, mas NAO colhe as boas
 		while not _todas_prontas():
-			megafazenda.paraleliza_blocos(_tarefa_ciclo())
-		# colhe tudo
+			megafazenda.paraleliza_blocos(_tarefa_planta_e_cuida())
+		# so colhe quando TUDO estiver maduro - bonus mega-abobora
 		megafazenda.paraleliza_blocos(_tarefa_colhe_tudo())
 		campo.ara()
