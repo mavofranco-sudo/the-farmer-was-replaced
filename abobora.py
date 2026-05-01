de71e5df90971ea05f68e4de20892611c402bca9
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 16:34:46 -0300

    refactor: reescreve abobora.py do zero sem campo.agua

--- abobora.py
+++ abobora.py
@@ -16,45 +16,37 @@ def _planta_abobora():
 	if num_items(Items.Water) > 0:
 		use_item(Items.Water)
 
-# FASE 1: planta em todas as celulas (primeira vez)
 def inicializa():
 	global _fila
 
 	_planta_abobora()
 	_fila["enfila"]((get_pos_x(), get_pos_y()))
 
-# FASE 2: percorre o campo checando mortas e nao prontas
 def _verifica_celula(x, y):
 	global _fila
 
 	tipo = get_entity_type()
 	if tipo == Entities.Dead_Pumpkin:
-		# replanta em cima da morta (remove automaticamente)
 		_planta_abobora()
 		_fila["enfila"]((x, y))
 	elif not can_harvest():
-		# ainda crescendo, volta pra fila
 		_fila["enfila"]((x, y))
 
 def _aguarda_e_replanta():
 	global _fila
 
 	_fila = fila.inicializa()
-	# percorre campo inteiro (NxN) plantando em cada celula
 	campo.movimento(inicializa)
 
-	# loop ate todas estarem prontas
 	while not _fila["vazia"]():
 		x, y = _fila["desenfila"]()
 		campo.vai_para(x, y)
 		_verifica_celula(x, y)
 
-# FASE 3: colhe a mega abobora percorrendo o campo inteiro
 def _colhe_mega():
 	campo.movimento(harvest)
 
 def _reabastece():
-	# cada celula do campo precisa de 1 semente = 512 cenouras
 	n_celulas = campo.n * campo.n
 	custo_por_semente = 512
 	margem = n_celulas * 2 + 100
@@ -75,4 +67,3 @@ def modo_abobora(objetivo):
 		_aguarda_e_replanta()
 		_colhe_mega()
 		campo.ara()
-
