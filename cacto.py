81d3b8109e6b7f19d0b2c82e0d635c3fee695295
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 17:46:30 -0300

    feat: paraleliza cacto.py com megafazenda - usa todos os drones disponiveis

--- cacto.py
+++ cacto.py
@@ -2,16 +2,44 @@ import campo
 import gerenciador
 import megafazenda
 
-def _till_ate_soil():
-	while get_ground_type() != Grounds.Soil:
-		till()
+def _tarefa_limpa():
+	def funcao():
+		def colhe_se_pronto():
+			if get_entity_type() != None and can_harvest():
+				harvest()
+		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, colhe_se_pronto)
+	return funcao
 
-def _celula_vazia():
-	return get_entity_type() == None
+def _tarefa_planta():
+	def funcao():
+		def planta_cacto():
+			tipo = get_entity_type()
+			if tipo != None:
+				if can_harvest():
+					harvest()
+				else:
+					return
+			campo.till_ate_soil()
+			if num_unlocked(Unlocks.Plant):
+				plant(Entities.Cactus)
+			campo._agua()
+		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, planta_cacto)
+	return funcao
 
-def _rega_celula():
-	if num_items(Items.Water) > 0:
-		use_item(Items.Water)
+def _tarefa_espera():
+	def funcao():
+		def rega():
+			campo._agua()
+		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, rega)
+	return funcao
+
+def _campo_todo_crescido():
+	for x in range(campo.n):
+		for y in range(campo.n):
+			campo.vai_para(x, y)
+			if get_entity_type() == Entities.Cactus and not can_harvest():
+				return False
+	return True
 
 def _measure_safe(direcao=None):
 	if direcao == None:
@@ -22,30 +50,16 @@ def _measure_safe(direcao=None):
 		return 0
 	return val
 
-def _vizinho_maduro(direcao):
-	val = measure(direcao)
-	if val == None:
-		return False
-	return True
-
-def _espera_crescer():
-	pronto = False
-	while not pronto:
-		pronto = True
-		for x in range(campo.n):
-			for y in range(campo.n):
-				campo.vai_para(x, y)
-				_rega_celula()
-				if not can_harvest():
-					pronto = False
-
 def _ordena_coluna(col):
 	trocou = True
 	while trocou:
 		trocou = False
 		for j in range(campo.n - 1):
 			campo.vai_para(col, j)
-			if _vizinho_maduro(North) and _measure_safe() > _measure_safe(North):
+			v_atual = _measure_safe()
+			v_norte = _measure_safe(North)
+			vizinho_existe = measure(North) != None
+			if vizinho_existe and v_atual > v_norte:
 				swap(North)
 				trocou = True
 
@@ -55,7 +69,10 @@ def _ordena_linha(lin):
 		trocou = False
 		for j in range(campo.n - 1):
 			campo.vai_para(j, lin)
-			if _vizinho_maduro(East) and _measure_safe() > _measure_safe(East):
+			v_atual = _measure_safe()
+			v_leste = _measure_safe(East)
+			vizinho_existe = measure(East) != None
+			if vizinho_existe and v_atual > v_leste:
 				swap(East)
 				trocou = True
 
@@ -66,37 +83,12 @@ def _ordena_campo():
 		for lin in range(campo.n):
 			_ordena_linha(lin)
 
-def _colhe_se_pronto():
-	if get_entity_type() != None and can_harvest():
-		harvest()
-
-def _limpa_campo():
-	def bloco():
-		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, _colhe_se_pronto)
-	megafazenda.paraleliza_blocos(bloco)
-
-def _planta_cacto():
-	if not _celula_vazia():
-		if can_harvest():
-			harvest()
-		else:
-			return
-	_till_ate_soil()
-	if num_unlocked(Unlocks.Plant):
-		plant(Entities.Cactus)
-	if num_items(Items.Water) > 0:
-		use_item(Items.Water)
-
-def _planta_campo():
-	def acao():
-		_planta_cacto()
-	campo.movimento(acao)
-
 def modo_cacto(objetivo):
 	while gerenciador.precisa(Items.Cactus, objetivo):
-		_limpa_campo()
-		_planta_campo()
-		_espera_crescer()
+		megafazenda.paraleliza_blocos(_tarefa_limpa())
+		megafazenda.paraleliza_blocos(_tarefa_planta())
+		while not _campo_todo_crescido():
+			megafazenda.paraleliza_blocos(_tarefa_espera())
 		_ordena_campo()
 		campo.vai_para(0, 0)
 		harvest()
