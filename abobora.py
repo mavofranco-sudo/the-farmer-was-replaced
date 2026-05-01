dd3d94ea313d34402e95ba3c3c07445be12db0dc
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 16:32:27 -0300

    fix: separa if inline em duas linhas para compatibilidade com o ambiente do jogo

--- abobora.py
+++ abobora.py
@@ -13,7 +13,8 @@ def _planta_abobora():
 	_till_ate_soil()
 	if num_unlocked(Unlocks.Plant):
 		plant(Entities.Pumpkin)
-	if num_items(Items.Water) > 0: use_item(Items.Water)
+	if num_items(Items.Water) > 0:
+		use_item(Items.Water)
 
 # FASE 1: planta em todas as celulas (primeira vez)
 def inicializa():
