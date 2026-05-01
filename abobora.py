89c1e67b302f2f604541283cdff7d5d252bbff12
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 17:58:53 -0300

    fix: abobora - tarefa unificada que replanta podres e rega em cada passagem

--- abobora.py
+++ abobora.py
@@ -2,45 +2,39 @@ import campo
 import gerenciador
 import megafazenda
 
-def _tarefa_planta():
+def _tarefa_ciclo():
 	def funcao():
-		def planta_celula():
+		def trata_celula():
 			tipo = get_entity_type()
-			if tipo == Entities.Dead_Pumpkin or tipo == None:
-				campo.colhe_e_cultiva_arado(Entities.Pumpkin)
+			if tipo == None:
+				campo.till_ate_soil()
+				if num_unlocked(Unlocks.Plant):
+					plant(Entities.Pumpkin)
+				campo._agua()
+			elif tipo == Entities.Dead_Pumpkin:
+				harvest()
+				campo.till_ate_soil()
+				if num_unlocked(Unlocks.Plant):
+					plant(Entities.Pumpkin)
+				campo._agua()
 			elif tipo == Entities.Pumpkin:
 				if can_harvest():
 					harvest()
-					campo.cultiva_arado(Entities.Pumpkin)
 				else:
 					campo._agua()
-		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, planta_celula)
+		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, trata_celula)
 	return funcao
 
-def _tarefa_espera():
-	def funcao():
-		def aguarda_e_rega():
-			if get_entity_type() == Entities.Pumpkin and not can_harvest():
-				campo._agua()
-		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, aguarda_e_rega)
-	return funcao
-
-def _tarefa_colhe():
-	def funcao():
-		def colhe_se_pronto():
-			if get_entity_type() == Entities.Pumpkin and can_harvest():
-				harvest()
-		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, colhe_se_pronto)
-	return funcao
-
-def _campo_todo_crescido():
+def _todas_prontas():
 	for x in range(campo.n):
 		for y in range(campo.n):
 			campo.vai_para(x, y)
 			tipo = get_entity_type()
-			if tipo == Entities.Pumpkin and not can_harvest():
+			if tipo == None:
 				return False
 			if tipo == Entities.Dead_Pumpkin:
+				return False
+			if tipo == Entities.Pumpkin and not can_harvest():
 				return False
 	return True
 
@@ -58,12 +52,23 @@ def _reabastece():
 	if num_items(Items.Carrot) < minimo_cenouras:
 		gerenciador.farma_recurso(Items.Carrot, minimo_cenouras)
 
+def _tarefa_colhe_tudo():
+	def funcao():
+		def colhe():
+			if get_entity_type() == Entities.Pumpkin and can_harvest():
+				harvest()
+		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, colhe)
+	return funcao
+
 def modo_abobora(objetivo):
 	campo.ara()
 	while gerenciador.precisa(Items.Pumpkin, objetivo):
 		_reabastece()
-		megafazenda.paraleliza_blocos(_tarefa_planta())
-		while not _campo_todo_crescido():
-			megafazenda.paraleliza_blocos(_tarefa_espera())
-		megafazenda.paraleliza_blocos(_tarefa_colhe())
+		# planta o campo todo
+		megafazenda.paraleliza_blocos(_tarefa_ciclo())
+		# aguarda crescimento - a cada passagem rega e replanta podres
+		while not _todas_prontas():
+			megafazenda.paraleliza_blocos(_tarefa_ciclo())
+		# colhe tudo
+		megafazenda.paraleliza_blocos(_tarefa_colhe_tudo())
 		campo.ara()
