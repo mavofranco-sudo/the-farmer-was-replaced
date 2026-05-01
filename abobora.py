b2ec52e53ac421e1593d186695581472438d2e53
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 16:00:28 -0300

    fix: _reabastece acumula minimo por rodada (512+50 cenouras) nao por campo inteiro

--- abobora.py
+++ abobora.py
@@ -32,16 +32,16 @@ def tarefa():
 		verifica(x, y)
 
 def _reabastece():
-	sementes = campo.n * campo.n
-	custo_carrot = sementes * 512 + 512
-	custo_wood = sementes * 2 + 50
-	custo_hay = sementes * 2 + 50
+	custo_por_semente = 512
+	minimo_cenouras = custo_por_semente + 50
+	custo_wood = minimo_cenouras * 2 + 50
+	custo_hay = minimo_cenouras * 2 + 50
 	if num_items(Items.Wood) < custo_wood:
 		gerenciador.farma_recurso(Items.Wood, custo_wood)
 	if num_items(Items.Hay) < custo_hay:
 		gerenciador.farma_recurso(Items.Hay, custo_hay)
-	if num_items(Items.Carrot) < custo_carrot:
-		gerenciador.farma_recurso(Items.Carrot, custo_carrot)
+	if num_items(Items.Carrot) < minimo_cenouras:
+		gerenciador.farma_recurso(Items.Carrot, minimo_cenouras)
 
 def modo_abobora(objetivo):
 	while gerenciador.precisa(Items.Pumpkin, objetivo):
