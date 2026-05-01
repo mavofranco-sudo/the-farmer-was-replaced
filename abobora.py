6cab08e1deb97dc75e2f362a7df488cd1104959b
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 18:40:32 -0300

    fix: remove dependencia gerenciador, reinicializa megafazenda, checa cenouras antes de plantar

--- abobora.py
+++ abobora.py
@@ -1,21 +1,25 @@
 import campo
-import gerenciador
 import megafazenda
+import policultura
+
+_CUSTO_SEMENTE = 512
 
 def _tarefa_planta_e_cuida():
 	def funcao():
 		def trata_celula():
 			tipo = get_entity_type()
 			if tipo == None:
-				campo.till_ate_soil()
-				if num_unlocked(Unlocks.Plant):
-					plant(Entities.Pumpkin)
+				if num_items(Items.Carrot) >= _CUSTO_SEMENTE:
+					campo.till_ate_soil()
+					if num_unlocked(Unlocks.Plant):
+						plant(Entities.Pumpkin)
 				campo._agua()
 			elif tipo == Entities.Dead_Pumpkin:
 				harvest()
-				campo.till_ate_soil()
-				if num_unlocked(Unlocks.Plant):
-					plant(Entities.Pumpkin)
+				if num_items(Items.Carrot) >= _CUSTO_SEMENTE:
+					campo.till_ate_soil()
+					if num_unlocked(Unlocks.Plant):
+						plant(Entities.Pumpkin)
 				campo._agua()
 			elif tipo == Entities.Pumpkin:
 				if not can_harvest():
@@ -44,29 +48,36 @@ def _tarefa_colhe_tudo():
 		campo.movimento_bloco(megafazenda.linhas, megafazenda.colunas, colhe)
 	return funcao
 
-def _reabastece():
+def _noop():
+	pass
+
+def _reabastece_insumos():
 	n_celulas = campo.n * campo.n
-	custo_por_semente = 512
-	margem = n_celulas * 2 + 100
-	minimo_cenouras = custo_por_semente * n_celulas + margem
-	custo_wood = minimo_cenouras * 2 + margem
-	custo_hay = minimo_cenouras * 2 + margem
-	if num_items(Items.Wood) < custo_wood:
-		gerenciador.farma_recurso(Items.Wood, custo_wood)
-	if num_items(Items.Hay) < custo_hay:
-		gerenciador.farma_recurso(Items.Hay, custo_hay)
+	minimo_cenouras = _CUSTO_SEMENTE * n_celulas + n_celulas * 2 + 100
+	margem = minimo_cenouras * 2 + 100
+
+	if num_items(Items.Wood) < margem:
+		if num_unlocked(Unlocks.Trees) > 0:
+			policultura.cria_modo_policultura(Items.Wood, Entities.Tree)(margem)
+		else:
+			policultura.cria_modo_policultura(Items.Wood, Entities.Bush)(margem)
+
+	if num_items(Items.Hay) < margem:
+		policultura.cria_modo_policultura(Items.Hay, Entities.Grass)(margem)
+
 	if num_items(Items.Carrot) < minimo_cenouras:
-		gerenciador.farma_recurso(Items.Carrot, minimo_cenouras)
+		policultura.cria_modo_policultura_com_reabastecimento(
+			Items.Carrot, Entities.Carrot, _noop
+		)(minimo_cenouras)
 
 def modo_abobora(objetivo):
+	campo.inicializa()
+	megafazenda.inicializa()
 	campo.ara()
-	while gerenciador.precisa(Items.Pumpkin, objetivo):
-		_reabastece()
-		# planta o campo todo
+	while num_items(Items.Pumpkin) < objetivo:
+		_reabastece_insumos()
 		megafazenda.paraleliza_blocos(_tarefa_planta_e_cuida())
-		# aguarda: rega e replanta podres, mas NAO colhe as boas
 		while not _todas_prontas():
 			megafazenda.paraleliza_blocos(_tarefa_planta_e_cuida())
-		# so colhe quando TUDO estiver maduro - bonus mega-abobora
 		megafazenda.paraleliza_blocos(_tarefa_colhe_tudo())
 		campo.ara()
