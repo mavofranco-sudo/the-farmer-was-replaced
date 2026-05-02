6ded43ccaead988cab3d8474608c4b1b054c75c5
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 22:21:46 -0300

    perf: colheita em faixas por drone (nd faixas, nao 1024 drones)

--- girassol_v2.py
+++ girassol_v2.py
@@ -7,7 +7,6 @@ _girassois = {}
 def _inicializa_celula():
 	tipo = get_entity_type()
 	if tipo == Entities.Sunflower:
-		# ja tem girassol - so mede e registra se maduro
 		if can_harvest():
 			p = measure()
 			if p == None:
@@ -16,7 +15,6 @@ def _inicializa_celula():
 		else:
 			campo._agua()
 		return
-	# celula vazia ou outra planta - colhe se tiver, ara e planta
 	if tipo != None:
 		if can_harvest():
 			harvest()
@@ -41,11 +39,82 @@ def _constroi_resultados(resultados):
 				_girassois[i].add(par)
 			i += 1
 
-def _tarefa_colheita(x, y):
+# cada drone recebe sua faixa de celulas e colhe na ordem certa (15->7)
+# a lista global _faixas_colheita[drone_idx] = lista ordenada de [p, x, y]
+_faixas_colheita = {}
+
+def _tarefa_colheita_faixa(idx):
 	def funcao():
-		campo.vai_para(x, y)
-		campo.colhe()
+		faixa = _faixas_colheita[idx]
+		# ordena por petalas decrescente dentro da faixa
+		# insertion sort (sem sorted/lambda)
+		i = 1
+		while i < len(faixa):
+			chave = faixa[i]
+			j = i - 1
+			while j >= 0 and faixa[j][0] < chave[0]:
+				faixa[j + 1] = faixa[j]
+				j -= 1
+			faixa[j + 1] = chave
+			i += 1
+		for item in faixa:
+			campo.vai_para(item[1], item[2])
+			if get_entity_type() == Entities.Sunflower:
+				if can_harvest():
+					harvest()
 	return chapeus.usa_e_faz(funcao)
+
+def _colhe_paralelo():
+	# monta lista global com todas as celulas maduras: [p, x, y]
+	todas = []
+	p = 15
+	while p >= 7:
+		for par in _girassois[p]:
+			todas.append([p, par[0], par[1]])
+		p -= 1
+
+	total = len(todas)
+	if total == 0:
+		return 0
+
+	nd = max_drones()
+	if nd < 1:
+		nd = 1
+
+	# divide em nd faixas
+	tam = total // nd
+	if tam < 1:
+		tam = 1
+
+	i = 0
+	while i < nd:
+		inicio = i * tam
+		fim = inicio + tam
+		if i == nd - 1:
+			fim = total
+		if inicio < total:
+			_faixas_colheita[i] = todas[inicio:fim]
+		else:
+			_faixas_colheita[i] = []
+		i += 1
+
+	drones = []
+	i = 0
+	while i < nd:
+		if len(_faixas_colheita[i]) > 0:
+			primeiro = _faixas_colheita[i][0]
+			campo.vai_para(primeiro[1], primeiro[2])
+			drone = spawn_drone(_tarefa_colheita_faixa(i))
+			if drone:
+				drones.append(drone)
+			else:
+				_tarefa_colheita_faixa(i)()
+		i += 1
+
+	for d in drones:
+		wait_for(d)
+
+	return total
 
 def tem_cenouras_suficientes():
 	tam = get_world_size()
@@ -66,28 +135,15 @@ def modo_girassol(objetivo):
 			return
 
 		t0 = get_time()
-
 		resultados = megafazenda.paraleliza_linha(_tarefa_plantio)
 		_constroi_resultados(resultados)
-
 		t_plantio = get_time()
 
-		total = 0
+		total = _colhe_paralelo()
+
+		# limpa para proximo ciclo
 		p = 15
 		while p >= 7:
-			if len(_girassois[p]) > 0:
-				drones = []
-				for par in _girassois[p]:
-					x = par[0]
-					y = par[1]
-					campo.vai_para(campo.metade_n, campo.metade_n)
-					drone = None
-					while not drone:
-						drone = spawn_drone(_tarefa_colheita(x, y))
-					drones.append(drone)
-				for d in drones:
-					wait_for(d)
-				total += len(_girassois[p])
 			_girassois[p] = set()
 			p -= 1
 
@@ -101,8 +157,7 @@ def modo_girassol(objetivo):
 			por_min = (total * 60) // dur
 		print("    [girassol] ciclo=" + str(ciclos) +
 			" colhidos=" + str(total) +
-			" power=" + str(num_items(Items.Power)) + "/" + str(objetivo) +
-			" total=" + str(dur * 1000 // 1) + "ms" +
+			" power=" + str(num_items(Items.Power) // 1) + "/" + str(objetivo // 1) +
 			" plantio=" + str(dur_plantio * 1000 // 1) + "ms" +
 			" colh=" + str(dur_colh * 1000 // 1) + "ms" +
 			" ritmo=" + str(por_min) + "/min")
