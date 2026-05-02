c936bc57e38a0ed62abf12e09f03ca0957cc93d5
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 22:36:17 -0300

    test: modo continuo sem bonus - 32 drones plant+harvest+replant em loop

--- girassol_v2.py
+++ girassol_v2.py
@@ -38,64 +38,94 @@ def _constroi_resultados(resultados):
 				_girassois[i].add(par)
 			i += 1
 
-# colhe uma lista de (x,y) passada via closure
-def _cria_colheita_lista(celulas):
+# modo continuo: cada drone faz sua faixa em loop: colhe se maduro, replanta, agua
+# sem ordenacao global, sem waves, sem bonus 8x
+def _cria_loop_faixa(x_ini, x_fim, y_ini, y_fim, ciclos):
 	def funcao():
-		for par in celulas:
+		c = 0
+		while c < ciclos:
+			y = y_ini
+			while y < y_fim:
+				x = x_ini
+				while x < x_fim:
+					campo.vai_para(x, y)
+					tipo = get_entity_type()
+					if tipo == Entities.Sunflower:
+						if can_harvest():
+							harvest()
+							if get_ground_type() != Grounds.Soil:
+								till()
+							if num_items(Items.Carrot) > 0:
+								plant(Entities.Sunflower)
+								campo._agua()
+						else:
+							campo._agua()
+					elif tipo == None:
+						if get_ground_type() != Grounds.Soil:
+							till()
+						if num_items(Items.Carrot) > 0:
+							plant(Entities.Sunflower)
+							campo._agua()
+					x += 1
+				y += 1
+			c += 1
+	return funcao
+
+def _colhe_por_waves():
+	total = 0
+	p = 15
+	while p >= 7:
+		for par in _girassois[p]:
 			campo.vai_para(par[0], par[1])
 			if get_entity_type() == Entities.Sunflower:
 				if can_harvest():
 					harvest()
-	return funcao
-
-def _colhe_wave_paralelo(celulas):
-	# divide as celulas entre nd drones e espera todos terminarem
-	if len(celulas) == 0:
-		return
-	nd = max_drones()
-	if nd < 1:
-		nd = 1
-	tam = len(celulas) // nd
-	if tam < 1:
-		tam = 1
-	drones = []
-	i = 0
-	while i < nd:
-		inicio = i * tam
-		if i == nd - 1:
-			fim = len(celulas)
-		else:
-			fim = inicio + tam
-		if inicio >= len(celulas):
-			i += 1
-			continue
-		fatia = celulas[inicio:fim]
-		tarefa = _cria_colheita_lista(fatia)
-		drone = spawn_drone(tarefa)
-		if drone:
-			drones.append(drone)
-		else:
-			tarefa()
-		i += 1
-	for d in drones:
-		wait_for(d)
-
-def _colhe_por_waves():
-	# colhe nivel por nivel (15->7), esperando todos os drones de cada nivel
-	# isso garante o bonus 8x: nenhum p=14 e colhido antes de todos p=15
-	total = 0
-	p = 15
-	while p >= 7:
-		celulas = list(_girassois[p])
-		if len(celulas) > 0:
-			_colhe_wave_paralelo(celulas)
-			total += len(celulas)
+					total += 1
 		p -= 1
 	return total
 
 def tem_cenouras_suficientes():
 	tam = get_world_size()
 	return num_items(Items.Carrot) >= tam * tam
+
+def modo_continuo(objetivo, ciclos_por_drone):
+	# cada drone faz sua faixa por ciclos_por_drone voltas sem parar
+	tam = get_world_size()
+	nd = max_drones()
+	if nd < 1:
+		nd = 1
+	faixa = tam // nd
+	if faixa < 1:
+		faixa = 1
+
+	t0 = get_time()
+	drones = []
+	i = 0
+	while i < nd:
+		x_ini = i * faixa
+		x_fim = x_ini + faixa
+		if i == nd - 1:
+			x_fim = tam
+		tarefa = _cria_loop_faixa(x_ini, x_fim, 0, tam, ciclos_por_drone)
+		drone = spawn_drone(tarefa)
+		if drone:
+			drones.append(drone)
+		else:
+			tarefa()
+		i += 1
+
+	for d in drones:
+		wait_for(d)
+
+	t_fim = get_time()
+	dur = t_fim - t0
+	power_atual = num_items(Items.Power) // 1
+	por_min = 0
+	if dur > 0:
+		por_min = (power_atual * 60) // dur
+	print("    [girassol-continuo] power=" + str(power_atual) +
+		" dur=" + str(dur * 1000 // 1) + "ms" +
+		" ritmo~=" + str(por_min) + "/min")
 
 def modo_girassol(objetivo):
 	i = 7
@@ -111,28 +141,17 @@ def modo_girassol(objetivo):
 			return
 
 		t0 = get_time()
-		resultados = megafazenda.paraleliza_linha(_tarefa_plantio)
-		_constroi_resultados(resultados)
-		t_plantio = get_time()
 
-		total = _colhe_por_waves()
-
-		p = 15
-		while p >= 7:
-			_girassois[p] = set()
-			p -= 1
+		# usa modo continuo: 32 drones, 3 ciclos cada
+		modo_continuo(objetivo, 3)
 
 		t_fim = get_time()
 		ciclos += 1
 		dur = t_fim - t0
-		dur_plantio = t_plantio - t0
-		dur_colh = t_fim - t_plantio
 		por_min = 0
 		if dur > 0:
-			por_min = (total * 60) // dur
+			por_min = (num_items(Items.Power) * 60) // dur
 		print("    [girassol] ciclo=" + str(ciclos) +
-			" colhidos=" + str(total) +
 			" power=" + str(num_items(Items.Power) // 1) + "/" + str(objetivo // 1) +
-			" plantio=" + str(dur_plantio * 1000 // 1) + "ms" +
-			" colh=" + str(dur_colh * 1000 // 1) + "ms" +
-			" ritmo=" + str(por_min) + "/min")
+			" dur=" + str(dur * 1000 // 1) + "ms" +
+			" ritmo~=" + str(por_min) + "/min")
