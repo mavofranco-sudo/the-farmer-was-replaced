24f675fb92a885d7a9512436e9c075e808b5d384
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 22:08:13 -0300

    refactor: girassol_v2 espelha logica da xTecna - paraleliza_linha retorna dict, colheita por drone por celula

--- girassol_v2.py
+++ girassol_v2.py
@@ -9,182 +9,84 @@ def _inicializa_celula():
 	p = measure()
 	if p == None:
 		p = 7
-	return [p, get_pos_x(), get_pos_y()]
+	_girassois[p].add((get_pos_x(), get_pos_y()))
 
-def _tarefa_linha():
-	tam = get_world_size()
-	resultado = {}
-	i = 7
-	while i <= 15:
-		resultado[i] = []
-		i += 1
-	x = 0
-	while x < tam:
-		campo.vai_para(x, get_pos_y())
-		campo.cultiva(Entities.Sunflower)
-		if get_entity_type() == Entities.Sunflower:
-			if can_harvest():
-				p = measure()
-				if p == None:
-					p = 7
-				if p < 7:
-					p = 7
-				if p > 15:
-					p = 15
-				resultado[p].append([get_pos_x(), get_pos_y()])
-		x += 1
-	return resultado
+def _tarefa_plantio():
+	campo.movimento_linha(_inicializa_celula)
+	return _girassois
 
-def _constroi_grupos(resultados, grupos):
+def _constroi_resultados(resultados):
 	for resultado in resultados:
 		if resultado == None:
 			continue
 		i = 7
 		while i <= 15:
-			for item in resultado[i]:
-				grupos[i].append(item)
+			for par in resultado[i]:
+				_girassois[i].add(par)
 			i += 1
 
 def _tarefa_colheita(x, y):
 	def funcao():
 		campo.vai_para(x, y)
-		if get_entity_type() == Entities.Sunflower:
-			if can_harvest():
-				harvest()
+		campo.colhe()
 	return chapeus.usa_e_faz(funcao)
 
 def tem_cenouras_suficientes():
 	tam = get_world_size()
 	return num_items(Items.Carrot) >= tam * tam
 
-def um_ciclo_girassol():
-	if not tem_cenouras_suficientes():
-		return False
-
-	t0 = get_time()
-
-	# planta e coleta medicoes por linha em paralelo
-	resultados = megafazenda.paraleliza_linha(_tarefa_linha)
-
-	t_plantio = get_time()
-
-	# consolida grupos por numero de petalas
-	grupos = {}
-	i = 7
-	while i <= 15:
-		grupos[i] = []
-		i += 1
-	_constroi_grupos(resultados, grupos)
-
-	# verifica se tem girassois maduros
-	total = 0
-	i = 7
-	while i <= 15:
-		total += len(grupos[i])
-		i += 1
-
-	if total == 0:
-		# nenhum maduro ainda - aguarda e tenta de novo sem replantio
-		while not _todos_maduros():
-			pass
-		t_crescido = get_time()
-		# recoleta apos crescer
-		i = 7
-		while i <= 15:
-			grupos[i] = []
-			i += 1
-		_coleta_serie(grupos)
-		total = 0
-		i = 7
-		while i <= 15:
-			total += len(grupos[i])
-			i += 1
-	else:
-		t_crescido = t_plantio
-
-	# colhe do maior para o menor - preserva bonus 8x
-	p = 15
-	while p >= 7:
-		if len(grupos[p]) > 0:
-			drones = []
-			for item in grupos[p]:
-				x = item[0]
-				y = item[1]
-				drone = spawn_drone(_tarefa_colheita(x, y))
-				if drone:
-					drones.append(drone)
-				else:
-					campo.vai_para(x, y)
-					if get_entity_type() == Entities.Sunflower:
-						if can_harvest():
-							harvest()
-			for d in drones:
-				wait_for(d)
-		p -= 1
-
-	t_fim = get_time()
-	dur_total = t_fim - t0
-	dur_plantio = t_plantio - t0
-	dur_cresc = t_crescido - t_plantio
-	dur_colh = t_fim - t_crescido
-	por_min = 0
-	if dur_total > 0:
-		por_min = (total * 60) // dur_total
-	print("    [girassol] colhidos=" + str(total) +
-		" total=" + str(dur_total * 1000 // 1) + "ms" +
-		" plantio=" + str(dur_plantio * 1000 // 1) + "ms" +
-		" cresc=" + str(dur_cresc * 1000 // 1) + "ms" +
-		" colh=" + str(dur_colh * 1000 // 1) + "ms" +
-		" ritmo=" + str(por_min) + "/min")
-	return True
-
-def _todos_maduros():
-	tam = get_world_size()
-	x = 0
-	while x < tam:
-		y = 0
-		while y < tam:
-			campo.vai_para(x, y)
-			tipo = get_entity_type()
-			if tipo == None:
-				campo.till_ate_soil()
-				if num_items(Items.Carrot) > 0:
-					plant(Entities.Sunflower)
-				return False
-			if tipo == Entities.Sunflower:
-				if not can_harvest():
-					campo._agua()
-					return False
-			y += 1
-		x += 1
-	return True
-
-def _coleta_serie(grupos):
-	tam = get_world_size()
-	x = 0
-	while x < tam:
-		y = 0
-		while y < tam:
-			campo.vai_para(x, y)
-			if get_entity_type() == Entities.Sunflower:
-				if can_harvest():
-					p = measure()
-					if p == None:
-						p = 7
-					if p < 7:
-						p = 7
-					if p > 15:
-						p = 15
-					grupos[p].append([x, y])
-			y += 1
-		x += 1
-
 def modo_girassol(objetivo):
+	i = 7
+	while i <= 15:
+		_girassois[i] = set()
+		i += 1
+
+	t_total_inicio = get_time()
+	ciclos = 0
+
 	while num_items(Items.Power) < objetivo:
 		if not tem_cenouras_suficientes():
 			print("    [girassol] sem cenouras (power=" + str(num_items(Items.Power)) + " obj=" + str(objetivo) + ")")
 			return
-		ok = um_ciclo_girassol()
-		if not ok:
-			print("    [girassol] ciclo falhou, abortando")
-			return
+
+		t0 = get_time()
+
+		resultados = megafazenda.paraleliza_linha(_tarefa_plantio)
+		_constroi_resultados(resultados)
+
+		t_plantio = get_time()
+
+		total = 0
+		p = 15
+		while p >= 7:
+			if len(_girassois[p]) > 0:
+				drones = []
+				for par in _girassois[p]:
+					x = par[0]
+					y = par[1]
+					campo.vai_para(campo.metade_n, campo.metade_n)
+					drone = None
+					while not drone:
+						drone = spawn_drone(_tarefa_colheita(x, y))
+					drones.append(drone)
+				for d in drones:
+					wait_for(d)
+				total += len(_girassois[p])
+			_girassois[p] = set()
+			p -= 1
+
+		t_fim = get_time()
+		ciclos += 1
+		dur = t_fim - t0
+		dur_plantio = t_plantio - t0
+		dur_colh = t_fim - t_plantio
+		por_min = 0
+		if dur > 0:
+			por_min = (total * 60) // dur
+		print("    [girassol] ciclo=" + str(ciclos) +
+			" colhidos=" + str(total) +
+			" power=" + str(num_items(Items.Power)) + "/" + str(objetivo) +
+			" total=" + str(dur * 1000 // 1) + "ms" +
+			" plantio=" + str(dur_plantio * 1000 // 1) + "ms" +
+			" colh=" + str(dur_colh * 1000 // 1) + "ms" +
+			" ritmo=" + str(por_min) + "/min")
