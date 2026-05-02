d9fb853024d195fb64478eb9f91ae1824a4c13a0
mavofranco-sudo <mavofranco@gmail.com>
2026-05-01 22:15:06 -0300

    feat: farm_power_infinito com meta 12000/min usando girassol_v2

--- main.py
+++ main.py
@@ -78,6 +78,49 @@ def farm_aboboras_infinito(objetivo):
 		abobora.modo_abobora(num_items(Items.Pumpkin) + get_world_size() * get_world_size())
 	print(">>> OBJETIVO ATINGIDO: " + str(num_items(Items.Pumpkin)) + " aboboras")
 
+def farm_power_infinito():
+	print(">>> MODO FARM POWER: objetivo 12000/min")
+	if not girassol_v2.tem_cenouras_suficientes():
+		n = get_world_size()
+		minimo_cenoura = n * n * 3
+		print("  [power] preparando cenouras: " + str(minimo_cenoura))
+		policultura.cria_modo_policultura_com_reabastecimento(
+			Items.Carrot, Entities.Carrot, _noop
+		)(minimo_cenoura)
+
+	ciclo = 0
+	t_inicio = get_time()
+	power_inicio = num_items(Items.Power)
+
+	while True:
+		ciclo += 1
+		if not girassol_v2.tem_cenouras_suficientes():
+			n = get_world_size()
+			minimo_cenoura = n * n * 3
+			policultura.cria_modo_policultura_com_reabastecimento(
+				Items.Carrot, Entities.Carrot, _noop
+			)(minimo_cenoura)
+
+		power_antes = num_items(Items.Power)
+		girassol_v2.modo_girassol(power_antes + get_world_size() * get_world_size())
+		power_depois = num_items(Items.Power)
+
+		t_agora = get_time()
+		dur_total = t_agora - t_inicio
+		power_total = power_depois - power_inicio
+		ritmo = 0
+		if dur_total > 0:
+			ritmo = (power_total * 60) // dur_total
+
+		print("  [power] ciclo=" + str(ciclo) +
+			" power=" + str(power_depois) +
+			" gerado=" + str(power_depois - power_antes) +
+			" ritmo=" + str(ritmo) + "/min")
+
+		if ciclo >= 10 and ritmo >= 12000:
+			print(">>> OBJETIVO ATINGIDO: " + str(ritmo) + "/min (meta: 12000/min)")
+			return
+
 clear()
 inicializa()
 
@@ -157,3 +200,7 @@ for conquista in ordem:
 
 # fase final: farm de 100 milhoes de aboboras
 farm_aboboras_infinito(100000000)
+
+# farm de power apos aboboras
+farm_power_infinito()
+
